96f9d6ac8e1c4229c8bc87628818e4b14e2ede92
José Henrique <jose.henrique@umdev.cloud>
2026-05-04 16:41:42 -0300

    feat: add vectordb package with hnsw index build and search

--- internal/vectordb/hnsw.go
+++ internal/vectordb/hnsw.go
@@ -1,1 +1,47 @@
 package vectordb
+
+import (
+	"math"
+
+	"github.com/coder/hnsw"
+	"github.com/josehenrique-dev/rinha-2026/internal/loader"
+)
+
+type Index struct {
+	graph *hnsw.Graph[uint32]
+	ds    *loader.Dataset
+}
+
+func euclidean(a, b []float32) float32 {
+	var sum float32
+	for i := range a {
+		d := a[i] - b[i]
+		sum += d * d
+	}
+	return float32(math.Sqrt(float64(sum)))
+}
+
+func Build(ds *loader.Dataset) (*Index, error) {
+	g := hnsw.NewGraph[uint32]()
+	g.M = 16
+	g.Distance = euclidean
+
+	for i := 0; i < ds.Count; i++ {
+		vec := ds.Vectors[i*ds.Dim : i*ds.Dim+ds.Dim]
+		g.Add(hnsw.MakeNode(uint32(i), vec))
+	}
+
+	return &Index{graph: g, ds: ds}, nil
+}
+
+func (idx *Index) Search(query [14]float32, k int) float32 {
+	neighbors := idx.graph.Search(query[:], k)
+
+	fraudCount := 0
+	for _, n := range neighbors {
+		if idx.ds.Labels[n.Key] == 1 {
+			fraudCount++
+		}
+	}
+	return float32(fraudCount) / float32(k)
+}
