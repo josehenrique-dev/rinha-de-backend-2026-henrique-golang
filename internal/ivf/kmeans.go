ec2e1dd61cab5c726273b90e0f8cfd9330767cc1
José Henrique <jose.henrique@umdev.cloud>
2026-05-06 13:42:45 -0300

    perf: nclusters 2048, parallel k-means on all 3m vectors, expand probes 10/50 for 1-4 fraud results

--- internal/ivf/kmeans.go
+++ internal/ivf/kmeans.go
@@ -1,62 +1,113 @@
 package ivf
 
-import "math/rand"
+import (
+	"runtime"
+	"sync"
+)
 
-func trainCentroids(vectors []float32, nVectors int, rng *rand.Rand) [NClusters][Dim]float32 {
-	sampleSize := nVectors
-	if sampleSize > 200000 {
-		sampleSize = 200000
+// assignParallel assigns every vector to its nearest centroid in parallel.
+func assignParallel(vectors []float32, nVectors int, centroids *[NClusters][Dim]float32, assignments []uint16) {
+	nWorkers := runtime.NumCPU()
+	if nWorkers < 1 {
+		nWorkers = 1
 	}
-
-	perm := make([]int, sampleSize)
-	for i := range perm {
-		perm[i] = rng.Intn(nVectors)
-	}
-
-	var centroids [NClusters][Dim]float32
-	for c := 0; c < NClusters; c++ {
-		vi := perm[c%sampleSize]
-		copy(centroids[c][:], vectors[vi*Dim:(vi+1)*Dim])
-	}
-
-	assignments := make([]int16, sampleSize)
-	var sums [NClusters][Dim]float64
-	var counts [NClusters]int32
-
-	for iter := 0; iter < 20; iter++ {
-		for si, vi := range perm {
-			vec := vectors[vi*Dim : (vi+1)*Dim]
-			best, bestD := 0, float32(1e38)
-			for c := 0; c < NClusters; c++ {
-				d := distSliceCent(vec, &centroids[c])
-				if d < bestD {
-					bestD = d
-					best = c
+	chunkSize := (nVectors + nWorkers - 1) / nWorkers
+	var wg sync.WaitGroup
+	for w := 0; w < nWorkers; w++ {
+		wg.Add(1)
+		go func(w int) {
+			defer wg.Done()
+			start := w * chunkSize
+			end := start + chunkSize
+			if end > nVectors {
+				end = nVectors
+			}
+			for i := start; i < end; i++ {
+				vec := vectors[i*Dim : (i+1)*Dim]
+				best, bestD := 0, float32(1e38)
+				for c := 0; c < NClusters; c++ {
+					d := distSliceCent(vec, &centroids[c])
+					if d < bestD {
+						bestD = d
+						best = c
+					}
 				}
+				assignments[i] = uint16(best)
 			}
-			assignments[si] = int16(best)
-		}
+		}(w)
+	}
+	wg.Wait()
+}
 
-		for c := range sums {
-			sums[c] = [Dim]float64{}
-			counts[c] = 0
+func trainCentroids(vectors []float32, nVectors int) [NClusters][Dim]float32 {
+	centroids := initCentroids(vectors, nVectors)
+	assignments := make([]uint16, nVectors)
+
+	nWorkers := runtime.NumCPU()
+	if nWorkers < 1 {
+		nWorkers = 1
+	}
+
+	type localAcc struct {
+		sums   [NClusters][Dim]float64
+		counts [NClusters]int32
+	}
+	accs := make([]*localAcc, nWorkers)
+	for i := range accs {
+		accs[i] = new(localAcc)
+	}
+
+	chunkSize := (nVectors + nWorkers - 1) / nWorkers
+
+	for iter := 0; iter < 15; iter++ {
+		var wg sync.WaitGroup
+		for w := 0; w < nWorkers; w++ {
+			wg.Add(1)
+			go func(w int) {
+				defer wg.Done()
+				start := w * chunkSize
+				end := start + chunkSize
+				if end > nVectors {
+					end = nVectors
+				}
+				acc := accs[w]
+				for c := range acc.sums {
+					acc.sums[c] = [Dim]float64{}
+					acc.counts[c] = 0
+				}
+				for i := start; i < end; i++ {
+					vec := vectors[i*Dim : (i+1)*Dim]
+					best, bestD := 0, float32(1e38)
+					for c := 0; c < NClusters; c++ {
+						d := distSliceCent(vec, &centroids[c])
+						if d < bestD {
+							bestD = d
+							best = c
+						}
+					}
+					assignments[i] = uint16(best)
+					for d := 0; d < Dim; d++ {
+						acc.sums[best][d] += float64(vec[d])
+					}
+					acc.counts[best]++
+				}
+			}(w)
 		}
-		for si, vi := range perm {
-			c := assignments[si]
-			vec := vectors[vi*Dim : (vi+1)*Dim]
-			for d := 0; d < Dim; d++ {
-				sums[c][d] += float64(vec[d])
-			}
-			counts[c]++
-		}
+		wg.Wait()
+
 		for c := 0; c < NClusters; c++ {
-			if counts[c] > 0 {
+			var sumD [Dim]float64
+			var count int64
+			for _, acc := range accs {
+				count += int64(acc.counts[c])
 				for d := 0; d < Dim; d++ {
-					centroids[c][d] = float32(sums[c][d] / float64(counts[c]))
+					sumD[d] += acc.sums[c][d]
 				}
-			} else {
-				vi := perm[rng.Intn(sampleSize)]
-				copy(centroids[c][:], vectors[vi*Dim:(vi+1)*Dim])
+			}
+			if count > 0 {
+				for d := 0; d < Dim; d++ {
+					centroids[c][d] = float32(sumD[d] / float64(count))
+				}
 			}
 		}
 	}
@@ -64,12 +115,37 @@ func trainCentroids(vectors []float32, nVectors int, rng *rand.Rand) [NClusters]
 	return centroids
 }
 
+func initCentroids(vectors []float32, nVectors int) [NClusters][Dim]float32 {
+	var centroids [NClusters][Dim]float32
+	step := nVectors / NClusters
+	if step < 1 {
+		step = 1
+	}
+	for c := 0; c < NClusters; c++ {
+		vi := (c * step) % nVectors
+		copy(centroids[c][:], vectors[vi*Dim:(vi+1)*Dim])
+	}
+	return centroids
+}
+
 func distSliceCent(vec []float32, c *[Dim]float32) float32 {
 	_ = vec[Dim-1]
-	var s float32
-	for i := 0; i < Dim; i++ {
-		d := vec[i] - c[i]
-		s += d * d
-	}
-	return s
+	d0 := vec[0] - c[0]
+	d1 := vec[1] - c[1]
+	d2 := vec[2] - c[2]
+	d3 := vec[3] - c[3]
+	d4 := vec[4] - c[4]
+	d5 := vec[5] - c[5]
+	d6 := vec[6] - c[6]
+	d7 := vec[7] - c[7]
+	d8 := vec[8] - c[8]
+	d9 := vec[9] - c[9]
+	d10 := vec[10] - c[10]
+	d11 := vec[11] - c[11]
+	d12 := vec[12] - c[12]
+	d13 := vec[13] - c[13]
+	return d0*d0 + d1*d1 + d2*d2 + d3*d3 +
+		d4*d4 + d5*d5 + d6*d6 + d7*d7 +
+		d8*d8 + d9*d9 + d10*d10 + d11*d11 +
+		d12*d12 + d13*d13
 }
