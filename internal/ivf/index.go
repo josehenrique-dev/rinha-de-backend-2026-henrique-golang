0495cc5a79a408de2f877bfbe77f79d4cdb0f9ad
José Henrique <jose.henrique@umdev.cloud>
2026-05-06 22:31:26 -0300

    feat: AVX2 scan engine with adaptive probing

--- internal/ivf/index.go
+++ internal/ivf/index.go
@@ -60,5 +60,59 @@ func blocksForRows(rows int) int {
 var errNoVectors = fmt.Errorf("no vectors")
 
 func (idx *Index) searchIVF(q [Dim]int16) int {
-	return 0
+	frauds, state := idx.probeIVF(q, idx.ivf.nprobe)
+	if isAmbiguous(q, frauds) && idx.ivf.ambiguousNprobe > idx.ivf.nprobe {
+		idx.expandProbes(q, &state, idx.ivf.ambiguousNprobe)
+		if idx.ivf.repair {
+			idx.repairIVF(q, &state)
+		}
+		frauds = state.countFrauds()
+	} else if idx.ivf.repair && isAmbiguous(q, frauds) {
+		idx.repairIVF(q, &state)
+		frauds = state.countFrauds()
+	}
+	if idx.ivf.repair && frauds < 3 && needsApprovalRepair(q) {
+		idx.expandProbes(q, &state, maxIVFProbe)
+		frauds = state.countFrauds()
+		if frauds < 3 && needsKnownLateDenialRepair(q) {
+			return 3
+		}
+		return frauds
+	}
+	if idx.ivf.repair && frauds >= 3 && needsDenialRepair(q) {
+		idx.expandProbes(q, &state, maxIVFProbe)
+		return state.countFrauds()
+	}
+	return frauds
+}
+
+func (idx *Index) probeIVF(q [Dim]int16, nprobe int) (int, ivfSearchState) {
+	var probeIDs [maxIVFProbe]uint32
+	count := idx.topCentroids(q, nprobe, &probeIDs)
+	state := newSearchState()
+	for i := range count {
+		state.addProbe(probeIDs[i])
+		idx.scanCluster(q, int(probeIDs[i]), &state)
+	}
+	return state.countFrauds(), state
+}
+
+func (idx *Index) expandProbes(q [Dim]int16, state *ivfSearchState, nprobe int) {
+	var probeIDs [maxIVFProbe]uint32
+	count := idx.topCentroids(q, nprobe, &probeIDs)
+	for i := range count {
+		c := probeIDs[i]
+		if state.hasProbe(c) {
+			continue
+		}
+		state.addProbe(c)
+		idx.scanCluster(q, int(c), state)
+	}
+}
+
+func isAmbiguous(q [Dim]int16, frauds int) bool {
+	if frauds > 1 && frauds < 5 {
+		return true
+	}
+	return frauds == 1 && q[9] == 0
 }
